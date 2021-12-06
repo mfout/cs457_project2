7bbaa03e463d26378a5798861ca82a95a1bf3c4e
mfout <56805367+mfout@users.noreply.github.com>
2021-12-06 10:51:24 -0700

    Added mfout user info

--- ss.py
+++ ss.py
@@ -1,9 +1,9 @@
 ###############################################
-# Group Name  : XXXXXX
+# Group Name  : Tencent
 
-# Member1 Name: XXXXXX
-# Member1 SIS ID: XXXXXX
-# Member1 Login ID: XXXXXX
+# Member1 Name: Mason Fout
+# Member1 SIS ID: 831378374
+# Member1 Login ID: mfout
 
 # Member2 Name: XXXXXX
 # Member2 SIS ID: XXXXXX
