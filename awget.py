3509d64c786432bd16f0c9e00a5e2c0007ab1d87
mfout <56805367+mfout@users.noreply.github.com>
2021-12-06 10:53:20 -0700

    some initial awget code

--- awget.py
+++ awget.py
@@ -10,3 +10,32 @@
 # Member2 Login ID: XXXXXX
 ###############################################
 
+import sys
+
+#create a class?
+#class
+
+#url and optional chainfile name
+init(sys.argv[1], sys.argv[2])
+
+def init(url, chainFile="chaingang.txt"):
+    readFile(chainFile)
+    readURL(url)
+                
+def readFile(chainFile):
+    filename = "chaingang.txt"
+    try:
+        if chainFile != filename:
+            filename = chainFile
+
+        with open(filename, 'r') as file:
+            #lines is a list of strings for each line of the text
+            lines = file.readlines()
+
+
+    except:
+        print("Local file ", filename, " not found. Exiting program.")
+        sys.exit(1)
+
+def readURL(url):
+    
